d2867955ddbbf7aa2aee1cef625b40f6f7889ed0
root <root@localhost.localdomain>
2019-06-05 15:36:49 +0100

    Added some stuff

--- install.sh
+++ install.sh
@@ -2,8 +2,30 @@
 
 script_home="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#Setup directories
-mkdir -p ~/.tmux/plugins
+for i in zsh git vim curl python;
+do
+    which $i >/dev/null
+    if [[ $? != 0 ]]; then
+      echo "[!] $i not found please run installer script for platform"
+      exit 1
+    fi
+done
+
+touch ${HOME}/.custom_host_config
+
+#Oh my zsh
+if [[ ! -d ${HOME}/.oh-my-zsh ]]; then
+  echo "[+] Installing oh my zsh"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo "[!] Oh My Zsh already appears to be installed"
+fi
+
+for i in .tmux.conf .vimrc .zshrc;
+do
+	ln -sf ${script_home}/$i ${HOME}/$i
+	echo "[+] Symlinked $i to ${HOME}/$i"
+done
 
 # Clone tmux plugin manager
 echo "[+] Setting up tmux...."
@@ -21,18 +43,17 @@ else
 	echo "[+] Minpac already cloned..."
 fi
 
-for i in .tmux.conf .vimrc;
-do
-	ln -sf ${script_home}/$i ${HOME}/$i
-	echo "[+] Symlinked $i to ${HOME}/$i"
-done
-
 vim -c "PackUpdate" README.md
-echo "[+] Trying install YouCompleteMe..."
-cd ${HOME}/.vim/pack/minpac/start/YouCompleteMe
-git submodule update --init --recursive
-python install.py
-cd -
+
+if [[ ! -f ~/.vim/pack/minpac/start/YouCompleteMe/third_party/ycmd/ycm_core.so ]]; then
+  echo "[+] Trying install YouCompleteMe..."
+  cd ${HOME}/.vim/pack/minpac/start/YouCompleteMe
+  git submodule update --init --recursive
+  python install.py
+  cd -
+else
+  echo "[+] YouCompleteMe already seems to be built"
+fi
 
 echo "[+] You need to go to ~/.vim/pack/minpac/start/YouCompleteMe and run the"
 echo "[+] install.py script (needs cmake and build-essentials and python dev)"
---
+++ install_prereq_debian.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt update
+apt install git curl screenfetch python build-essential vim zsh tmux python-dev cmake
